0c30b70e64259a7c94be6e34588fa593a8517304
Duy Anh <duyanhvn10@gmail.com>
2026-06-12 11:18:03 +0700

    Khởi tạo dự án Example02

---
+++ src/main/java/com/nguyenduyanh/example02/entity/Product.java
@@ -0,0 +1,29 @@
+package com.nguyenduyanh.example02.entity;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.stereotype.Indexed;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name= "Products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false, unique = true)
+    private String photo;
+    @Column(nullable = false)
+    private double price;
+}
